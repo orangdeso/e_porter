59817eb99d2656f54b5a8da494f20e1bb370332d
orangdeso <ahmadzaqi98mmm@gmail.com>
2025-05-28 23:59:45 +0700

    Feat: add boarding screen

--- lib/presentation/screens/onboarding/onboarding_screen.dart
+++ lib/presentation/screens/onboarding/onboarding_screen.dart
@@ -1,9 +1,11 @@
 import 'package:e_porter/_core/component/button/button_fill.dart';
 import 'package:e_porter/_core/component/button/button_outline.dart';
 import 'package:e_porter/_core/constants/colors.dart';
+import 'package:e_porter/_core/constants/typography.dart';
 import 'package:e_porter/presentation/screens/routes/app_rountes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 import 'package:zoom_tap_animation/zoom_tap_animation.dart';
 
@@ -21,7 +23,19 @@ class OnboardingScreen extends StatelessWidget {
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
           child: Column(
-            children: [],
+            children: [
+              SvgPicture.asset(
+                'assets/images/ilustrasi_onboarding.svg',
+              ),
+              SizedBox(height: 16.h),
+              TypographyStyles.h6(
+                'Hemat waktu Anda di bandara dengan layanan E-Porter. Kami siap membantu setiap kebutuhan perjalanan prioritas Anda dengan mudah dan cepat',
+                fontWeight: FontWeight.w600,
+                color: GrayColors.gray600,
+                textAlign: TextAlign.center,
+                maxlines: 5,
+              )
+            ],
           ),
         ),
       ),
